95fa285915c556fed0bf094888c743a4ad9dcef7
unknown <anhductrannguyen170703@gmail.com>
2023-12-01 20:44:40 +0700

    first commit

---
+++ Session1.js
@@ -0,0 +1,133 @@
+import React from "react";
+import "./index.css"
+function Session1() {
+    return (
+        <div className="BackGrounPage">
+            <div className="AllInformationFromSessionFirst">
+                <div className="HeaderFromSessionFirst">
+                    <h2>
+                        Online courses
+                    </h2>
+                    <p>
+                        Be yourself at our center , a place where you can encounter the body , mind and spirt balancing and merging them into oneness
+                    </p>
+                </div>
+                <div className="AllOptionYoga">
+                    <div className="OptionOnYoga">
+                        <div className="AllimgOnOptionoga">
+                            <img src="https://tse4.mm.bing.net/th?id=OIP.PSX5MzxWE_OE8JeR639GSwHaE7&pid=Api&P=0&h=220" className="ImgOnOptionyoga"></img>
+                        </div>
+                        <div className="AllInformationOnOptionYoga">
+                            <h4 className="HeaderInOptionInyoga">Vinyasa flow yoga</h4>
+                            <p>Our aim is to share oga knowledge with the students and help them relax </p>
+                            <div className="PriceOnYoga">
+                                <p className="FirstPriceOnYoga">$245</p>
+                                <p className="SalePriceOnYoga">$200</p>
+                            </div>
+                            <div className="AllInFormationTrainingOnWeek">
+                                <div className="BorderInFormationTrainingOnWeek">
+                                    <p className="OptionWeekTraining">WEEK 1</p>
+                                    <p className="InFormationTrainingOnWeek">  Get started and blance</p>
+                                </div>
+                                <div className="BorderInFormationTrainingOnWeek">
+                                    <p className="OptionWeekTraining">WEEK 2</p>
+                                    <p className="InFormationTrainingOnWeek">Focus on the mind</p>
+                                </div>
+                                <div className="BorderInFormationTrainingOnWeek LastBorderInFormationTrainingOnWeek">
+                                    <p className="OptionWeekTraining">WEEK 3</p>
+                                    <p className="InFormationTrainingOnWeek"> Head relax yoga</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div><div className="OptionOnYoga">
+                        <div className="AllimgOnOptionoga">
+                            <img src="https://tse4.mm.bing.net/th?id=OIP.PSX5MzxWE_OE8JeR639GSwHaE7&pid=Api&P=0&h=220" className="ImgOnOptionyoga"></img>
+                        </div>
+                        <div className="AllInformationOnOptionYoga">
+                            <h4 className="HeaderInOptionInyoga">Vinyasa flow yoga</h4>
+                            <p>Our aim is to share oga knowledge with the students and help them relax </p>
+                            <div className="PriceOnYoga">
+                                <p className="FirstPriceOnYoga">$245</p>
+                                <p className="SalePriceOnYoga">$200</p>
+                            </div>
+                            <div className="AllInFormationTrainingOnWeek">
+                                <div className="BorderInFormationTrainingOnWeek">
+                                    <p className="OptionWeekTraining">WEEK 1</p>
+                                    <p className="InFormationTrainingOnWeek">  Get started and blance</p>
+                                </div>
+                                <div className="BorderInFormationTrainingOnWeek">
+                                    <p className="OptionWeekTraining">WEEK 2</p>
+                                    <p className="InFormationTrainingOnWeek">Focus on the mind</p>
+                                </div>
+                                <div className="BorderInFormationTrainingOnWeek LastBorderInFormationTrainingOnWeek">
+                                    <p className="OptionWeekTraining">WEEK 3</p>
+                                    <p className="InFormationTrainingOnWeek"> Head relax yoga</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="OptionOnYoga">
+                        <div className="AllimgOnOptionoga">
+                            <img src="https://tse4.mm.bing.net/th?id=OIP.PSX5MzxWE_OE8JeR639GSwHaE7&pid=Api&P=0&h=220" className="ImgOnOptionyoga"></img>
+                        </div>
+                        <div className="AllInformationOnOptionYoga">
+                            <h4 className="HeaderInOptionInyoga">Vinyasa flow yoga</h4>
+                            <p>Our aim is to share oga knowledge with the students and help them relax </p>
+                            <div className="PriceOnYoga">
+                                <p className="FirstPriceOnYoga">$245</p>
+                                <p className="SalePriceOnYoga">$200</p>
+                            </div>
+                            <div className="AllInFormationTrainingOnWeek">
+                                <div className="BorderInFormationTrainingOnWeek">
+                                    <p className="OptionWeekTraining">WEEK 1</p>
+                                    <p className="InFormationTrainingOnWeek">  Get started and blance</p>
+                                </div>
+                                <div className="BorderInFormationTrainingOnWeek">
+                                    <p className="OptionWeekTraining">WEEK 2</p>
+                                    <p className="InFormationTrainingOnWeek">Focus on the mind</p>
+                                </div>
+                                <div className="BorderInFormationTrainingOnWeek LastBorderInFormationTrainingOnWeek">
+                                    <p className="OptionWeekTraining">WEEK 3</p>
+                                    <p className="InFormationTrainingOnWeek"> Head relax yoga</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="OptionOnYoga">
+                        <div className="AllimgOnOptionoga">
+                            <img src="https://tse4.mm.bing.net/th?id=OIP.PSX5MzxWE_OE8JeR639GSwHaE7&pid=Api&P=0&h=220" className="ImgOnOptionyoga"></img>
+                        </div>
+                        <div className="AllInformationOnOptionYoga">
+                            <h4 className="HeaderInOptionInyoga">Vinyasa flow yoga</h4>
+                            <p>Our aim is to share oga knowledge with the students and help them relax </p>
+                            <div className="PriceOnYoga">
+                                <p className="FirstPriceOnYoga">$245</p>
+                                <p className="SalePriceOnYoga">$200</p>
+                            </div>
+                            <div className="AllInFormationTrainingOnWeek">
+                                <div className="BorderInFormationTrainingOnWeek">
+                                    <p className="OptionWeekTraining">WEEK 1</p>
+                                    <p className="InFormationTrainingOnWeek">  Get started and blance</p>
+                                </div>
+                                <div className="BorderInFormationTrainingOnWeek">
+                                    <p className="OptionWeekTraining">WEEK 2</p>
+                                    <p className="InFormationTrainingOnWeek">Focus on the mind</p>
+                                </div>
+                                <div className="BorderInFormationTrainingOnWeek LastBorderInFormationTrainingOnWeek">
+                                    <p className="OptionWeekTraining">WEEK 3</p>
+                                    <p className="InFormationTrainingOnWeek"> Head relax yoga</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Session1;
